98941223439ca3e73f423a1b54b314d19f033106
2051300204abc <15933850817@163.com>
2026-09-06 14:24:32 +0800

    第二次修改

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,8 +17,19 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("web.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        '/assets/',
+        document_root=settings.BASE_DIR / 'static/frontend/assets'
+    )
+    urlpatterns += static(
+        '/media/',
+        document_root=settings.MEDIA_ROOT
+    )
---
+++ vue-project/vite.config.ts
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  build: {
+    outDir: path.resolve(__dirname, '../backend/static/vue-project'), // 打包到 Django static
+    emptyOutDir: true,
+  },//把前端的文件打包到后端
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
